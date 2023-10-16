c1ffe8e171a38a512a33b7d9486d0b8e6e945d84
Himanshu Sah <himanshusah41@gmail.com>
2023-10-16 13:26:55 -0400

    Header completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes,  Route } from 'react-router-dom';
 import './App.css';
 import Login from './Components/Login';
+import Home from './Components/Home';
+import Header from './Components/Header';
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path = '/' element = {<Login />}/>
+          <Route path='/home' element = {<Header />} />
         </Routes>
       </Router>
     </div>
